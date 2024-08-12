14653f80d6274d38ff80e4bd2ff886fd827c1c6d
damithc <damithc@github>
2024-08-12 14:24:34 +0800

    Site Update. [skip ci]

--- schedule/week11/project.page-vue-render.js
+++ schedule/week11/project.page-vue-render.js
@@ -74,6 +74,6 @@ with(this){return _c('li',[_c('strong',[_v("You can do an "),_c('em',[_v("additi
 with(this){return _c('li',[_v("As before, wrap up the milestone "),_c('span',{staticClass:"dimmed"},[_v("(i.e., reschedule/close any remaining issues/PRs and close the milestone).")])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.2")]),_v(", generated on Sun, 11 Aug 2024, 0:38:09 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.2")]),_v(", generated on Mon, 12 Aug 2024, 14:18:48 GMT+8]")])])])}
 }];
   
