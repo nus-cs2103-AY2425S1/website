d2ed4515bc76d178b0b2a57f4c338a7a91fd6f5d
damithc <damithc@github>
2024-08-31 14:48:03 +0800

    Site Update. [skip ci]

--- book/oop/polymorphism/what/index.page-vue-render.js
+++ book/oop/polymorphism/what/index.page-vue-render.js
@@ -20,6 +20,6 @@ with(this){return _c('p',[_c('strong',[_c('em',[_v("Polymorphism")]),_v(" allows
 with(this){return _c('p',[_c('em',[_v("Polymorphism")]),_v(" literally means \"ability to take many forms\".")])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.2")]),_v(", generated on Sat, 31 Aug 2024, 1:12:11 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.2")]),_v(", generated on Sat, 31 Aug 2024, 14:42:17 GMT+8]")])])])}
 }];
   
