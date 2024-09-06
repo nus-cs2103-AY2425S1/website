350c4890814e09df39a12026585d115b5b375538
damithc <damithc@github>
2024-09-07 02:30:16 +0800

    Site Update. [skip ci]

--- book/projectPlanning/milestones/index.page-vue-render.js
+++ book/projectPlanning/milestones/index.page-vue-render.js
@@ -17,6 +17,6 @@ with(this){return _c('div',[_c('h4',{attrs:{"id":"milestones"}},[_c('span',[_v("
 with(this){return _c('p',[_c('strong',[_v("A "),_c('em',[_v("milestone")]),_v(" is the end of a stage which indicates significant progress.")]),_v(" You should take into account dependencies and priorities when deciding on the features to be delivered at a certain milestone.")])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.2")]),_v(", generated on Fri, 6 Sept 2024, 17:57:51 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.2")]),_v(", generated on Sat, 7 Sept 2024, 2:24:15 GMT+8]")])])])}
 }];
   
