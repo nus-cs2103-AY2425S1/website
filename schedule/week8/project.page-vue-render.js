cb1d36270ad66c3135308d1832505df61dc6658c
damithc <damithc@github>
2024-09-22 13:38:50 +0800

    Site Update. [skip ci]

--- schedule/week8/project.page-vue-render.js
+++ schedule/week8/project.page-vue-render.js
@@ -116,6 +116,6 @@ with(this){return _c('ul',{staticStyle:{"list-style-type":"none","padding-inline
 with(this){return _c('h4',{attrs:{"id":"end-of-tp-week-8"}},[_c('strong',[_c('strong',[_v("End of tP Week 8")])]),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#end-of-tp-week-8","onclick":"event.stopPropagation()"}})])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v(", generated on Fri, 20 Sept 2024, 19:03:32 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v(", generated on Sun, 22 Sept 2024, 13:32:41 GMT+8]")])])])}
 }];
   
